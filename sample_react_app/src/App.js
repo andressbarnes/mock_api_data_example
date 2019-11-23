6038185f435d95830e3a04b8047222ab039f5449
Andy Barnes <andressbarnes@gmail.com>
2019-11-22 17:26:43 -0800

    updates to project

--- sample_react_app/src/App.js
+++ sample_react_app/src/App.js
@@ -1,12 +1,31 @@
-import React from "react";
+import React, { Component } from "react";
+//import { Container, Segment } from "semantic-ui-react";
 import "./App.css";
+import jsonServer from "./api/json-server";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello!</h1>
-    </div>
-  );
+class App extends Component {
+  state = {
+    users: []
+  };
+
+  fetchData = async term => {
+    const response = await jsonServer.get(`users`, {
+      params: { query: term }
+    });
+    this.setState({ users: response });
+  };
+
+  componentDidMount() {
+    this.fetchData();
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Hello</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
